d7fb49d752eb3e5311722d7ff7e7e88ca1fe4719
Ictyospore <Ictyospore@protonmail.com>
2022-02-27 21:52:45 +0100

    Autowiring

--- conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -5,13 +5,18 @@ import java.util.List;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SpeakerServiceImpl implements SpeakerService {
     
     private SpeakerRepository repository;
 
-    
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
     
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,7 +24,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 
