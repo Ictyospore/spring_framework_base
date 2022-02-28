fbd7a037f9fdd208f2b61d688b647d39c05bbd33
Ictyospore <Ictyospore@protonmail.com>
2022-02-28 15:20:51 +0100

    setter & constructor injection

--- conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,15 +3,26 @@ package com.pluralsight.service;
 import java.util.List;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibrenateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 public class SpeakerServiceImpl implements SpeakerService {
     
-    private SpeakerRepository repository = new HibrenateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+        
+    }
     
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
     public List<Speaker> findAll(){
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 
 }
